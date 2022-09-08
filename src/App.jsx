ab85695d7a8030dd1a74da444cd2cc4ae8ca6a92
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2022-09-09 00:15:56 +0500

    passed object as props

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,7 @@ import data from "./data"
       return (
             <Card 
                 key={item.id}
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-                openSpots={item.openSpots}
+                item= {item}
             />
       )
     })
